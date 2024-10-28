e4dc8e91d61519c2e732ed82a56135f7c39e442d
changjjjjjjj <light9632@gmail.com>
2024-10-28 22:21:29 +0900

    Chore: add kakaomap place listing script - 장소 긁어오기용 스크립트

---
+++ scripts/get_dog_related_places.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REST_API_KEY=""
+LATITUDE=37.40270870047064
+LONGITUDE=127.10332989692688
+RADIUS=20000
+QUERY="강아지"
+
+echo "{" > temp.json
+echo "\"documents\": [" >> temp.json
+
+for PAGE in $(seq 1 45); do
+    RESPONSE=$(curl -s -G "https://dapi.kakao.com/v2/local/search/keyword.json?x=$LONGITUDE&y=$LATITUDE&radius=$RADIUS&page=$PAGE" \
+        -H "Authorization: KakaoAK $REST_API_KEY" \
+        --data-urlencode "query=$QUERY")
+
+    if [ "$FIRST_PAGE" = true ]; then
+        echo "$RESPONSE" | jq -c '.documents[]' >> temp.json
+        FIRST_PAGE=false
+    else
+        echo "$RESPONSE" | jq -c '.documents[]' | sed 's/^/,/' >> temp.json
+    fi
+done
+sed -i '' '3s/^,//' temp.json
+
+echo "]" >> temp.json
+echo "}" >> temp.json
+
+cat temp.json | jq '.documents | unique_by(.id) | sort_by(.category_name)' > place.json
+rm temp.json
